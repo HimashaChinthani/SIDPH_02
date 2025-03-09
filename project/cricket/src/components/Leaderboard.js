9626dd017c4a2ff6692e7f54c347eae506afa2e8
Pabodya89 <2022cs209@stu.ucsc.cmb.ac.lk>
2025-03-09 08:56:51 +0530

    "Login and Signup files"

---
+++ project/cricket/src/components/Leaderboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const Leaderboard = () => {
+  return (
+    <Container>
+      <Typography variant="h4">Leaderboard</Typography>
+      <p>View the rankings of top teams.</p>
+    </Container>
+  );
+};
+
+export default Leaderboard;
