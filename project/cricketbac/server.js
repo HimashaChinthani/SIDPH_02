97907a4fed7aec4b2bd6d1db56ab29ae206e5b9c
Pabodya89 <2022cs209@stu.ucsc.cmb.ac.lk>
2025-03-09 17:29:46 +0530

    "download the bycrypt package"

--- project/cricketbac/server.js
+++ project/cricketbac/server.js
@@ -3,13 +3,31 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const playerRoutes = require('./routes/players');
+require('dotenv').config();
+const tournamentSummary = require('./routes/TounamentSummary');
+
+const SECRET_KEY = process.env.JWT_SECRET;
+
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use('/api/auth', authRoutes);
-app.use('/api/players', playerRoutes);
 
-mongoose.connect('mongodb://localhost:27017/cricket', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
-  .catch(err => console.log(err));
+app.use(cors({
+  origin: 'http://localhost:3000', // or your frontend URL
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+}));
+
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => console.log('MongoDB Connected'))
+
+  .catch(err => console.error("Mogodb Connection error:",err));
+
+  app.use('/api/auth', authRoutes);
+  app.use('/api/players', playerRoutes);
+  app.use('/api/TounamentSummary', tournamentSummary);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
